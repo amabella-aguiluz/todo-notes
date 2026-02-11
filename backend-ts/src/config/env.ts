3cec2d654b04ff9f08a0696a209175fd4e36a30d
amabellaaguiluz-fullsuite <amabella.aguiluz@getfullsuite.com>
2026-02-11 11:13:24 +0800

    REFACTOR: Backup setup was fixed - Added tsconfig.json Added env.ts

---
+++ backend-ts/src/config/env.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const env = {
+    PORT: process.env.PORT,
+    BACKEND_URL: process.env.BACKEND_URL,
+    FRONTEND_URL_PROD: process.env.FRONTEND_URL_PROD,
+    FRONTEND_URL_DEV: process.env.FRONTEND_URL_DEV,
+};
+
+export default env;
