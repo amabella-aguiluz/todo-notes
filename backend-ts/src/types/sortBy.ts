562e9e81551bcb76397032da0b2cf7886e641351
amabellaaguiluz-fullsuite <amabella.aguiluz@getfullsuite.com>
2026-02-11 11:32:21 +0800

    REFACTOR: Rewrote note controllers to TypeScript format

--- backend-ts/src/types/sortBy.ts
+++ backend-ts/src/types/sortBy.ts
@@ -1,4 +1,8 @@
-export type sortBy =
-    | "title"
-    | "created_at"
-    | "updated_at";
+
+export const sort_fields = ["title", "created_at", "updated_at"] as const;
+export const order = ["asc", "desc"] as const;
+
+
+
+export type sortBy = (typeof sort_fields)[number];
+export type orderBy = (typeof order)[number];
