1d73e04706e587fdddc197ba6a131a9e4e4d5ae8
amabellaaguiluz-fullsuite <amabella.aguiluz@getfullsuite.com>
2026-02-16 15:57:52 +0800

    god help me

--- backend-js/utils/error.js
+++
@@ -1,8 +0,0 @@
-// error handling
-
-// default error message
-export const errorMsg = (res, err) => {
-     res.status(500).json({error: err.message});
-};
-
-export default errorMsg;
---
+++ backend-ts-2/src/config/env.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const env = {
+    PORT: process.env.PORT,
+    BACKEND_URL: process.env.BACKEND_URL,
+    FRONTEND_URL_PROD: process.env.FRONTEND_URL_PROD,
+    FRONTEND_URL_DEV: process.env.FRONTEND_URL_DEV,
+    JWT_SECRET: process.env.JWT_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+};
+
+export default env;
